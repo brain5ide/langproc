b61cd07ed97e1017aec18ef03735eff45db5fab3
Vytautas Aukstikalnis <vytautas@aukstikalnis.lt>
2013-12-16 03:17:58 +0200

    Algorithm to unscramble text when we know the original message

--- generate.py
+++ generate.py
@@ -26,11 +26,7 @@ def dam_lev_dist(s1, s2):
                 cost = 0
             else:
                 cost = 1
-            d[(i,j)] = min(
-                            d[(i-1,j)] + 1, # deletion
-                            d[(i,j-1)] + 1, # insertion
-                            d[(i-1,j-1)] + cost, # substitution
-                          )
+            d[(i,j)] = d[(i-1,j-1)] + cost # substitution
             if i and j and s1[i]==s2[j-1] and s1[i-1] == s2[j]:
                 d[(i, j)] = min (d[(i, j)], d[i-2, j-2] + cost) # transposition
     return d[lenstr1-1,lenstr2-1]
@@ -45,4 +41,20 @@ crypt_split = split_text[:]
 random.shuffle(crypt_split)
 crypt_text = ''.join(crypt_split) 
 
+def unscramble(plain, crypt):
+    plain = list(plain)
+    crypt = list(crypt)
+    lenplain = len(plain)
+    lencrypt = len(crypt)
 
+    for i in xrange(lencrypt):
+        for j in xrange(lencrypt):
+            before = dam_lev_dist(plain, crypt)
+            if before == 0:
+                return crypt
+            crypt[i], crypt[j] = crypt[j], crypt[i] 
+            after = dam_lev_dist(plain, crypt) 
+            if after >= before:
+                print 'before: ', before, ' after: ', after
+                crypt[i], crypt[j] = crypt[j], crypt[i]
+    return crypt
