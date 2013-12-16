e0be678443bd47794f5dc9699adfc86eeb227053
Vytautas Aukstikalnis <vytautas@aukstikalnis.lt>
2013-12-16 08:25:32 +0200

    Recognition of words almost works. TODO is marked with a TODO

--- generate.py
+++ generate.py
@@ -4,11 +4,14 @@ import random
 # regular expressions paketas patogiam skaidymui i tris dalis
 import re
 
+# itertools helps to create permutations
+import itertools
+
 # file that contains words
 wordfile = 'wordlists/EN.txt'
 
 # number of words to produce
-wordnum = 10
+wordnum = 10 
 
 # calculate modified Damerau-Levenshtein distance between to given strings
 # don't calculate insertion and deletion distance
@@ -37,9 +40,14 @@ def dam_lev_dist(s1, s2, trans=False):
 
 allwords = [w.strip() for w in open(wordfile, "rb").readlines()]
 
-words_subset = random.sample(allwords, wordnum*wordnum)
+words_subset = random.sample(allwords, wordnum*100)
 used_words = random.sample(words_subset, wordnum)
 plaintext = " ".join(used_words)
+if len(plaintext) % 3 == 1:
+    plaintext = plaintext+'  '
+
+if len(plaintext) % 3 == 2:
+    plaintext = plaintext+' '
 
 split_text = re.findall('...?', plaintext) 
 crypt_split = split_text[:]
@@ -72,3 +80,64 @@ def possible_words(wlist, crypt):
         if found:
             poss.append(word)
     return poss
+
+# can word be found in scrambled text
+def is_inscrambled(word, crypt):
+    num = triplets_match_word(word,crypt)
+    if len(num) >= ((len(word)/3)+1) :
+        perms = list(itertools.permutations(num)) 
+        for item in perms:
+            if re.match(".*"+word+".*",''.join(item)):
+                return True
+        return False
+    else:
+        return False
+
+# ignore triplest that have a spacebar in the middle '. .'
+def probable_words(wlist, crypt):
+    prob = []
+    for item in wlist:
+        if is_inscrambled(item,crypt):
+            prob.append(item)
+    return prob
+
+def actual_words(wlist, crypto):
+    crypt = crypto[:]
+    prob = probable_words(wlist, crypt)
+    prob.sort(lambda x,y: cmp(len(y), len(x)))
+    
+    actual_words = []
+    for word in prob:
+        if is_inscrambled(word, crypt):
+            actual_words.append(word)
+            for o in set(triplets_match_word(word, crypt)):
+                try:
+                    print 'Removing: ', o
+                    # TODO: problema. Istrina tripleta, kuris turi space'a.
+                    # problema tame, kad kitam zodziui jo nelieka
+                    crypt.remove(o)
+                except ValueError:
+                    pass
+    return actual_words
+
+def triplets_match_word(word, crypt):
+    cry = []
+    for item in crypt:
+        if triplet_matches_word(word,item):
+            cry.append(item)
+    return cry
+
+def triplet_matches_word(word, triplet):
+    if re.match(".*"+triplet+".*", word):
+        return True
+    if re.match(". "+word[0:1], triplet):
+        return True
+    if re.match(" "+word[0:2], triplet):
+        return True
+    if re.match(word[-2:]+" ", triplet):
+        return True
+    if re.match(word[-1:]+" .", triplet):
+        return True
+    if re.match(".*"+word+".*", triplet):
+        return True
+    return False
